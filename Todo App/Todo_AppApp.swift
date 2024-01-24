f77db819c6871dd5e628811e510ba22c05992398
Harald Kurz <harald.kurz1988@gmail.com>
2024-01-24 18:24:53 +0100

    Initial Commit

---
+++ Todo App/Todo_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  Todo_AppApp.swift
+//  Todo App
+//
+//  Created by Harald Kurz on 24.01.24.
+//
+
+import SwiftUI
+
+@main
+struct Todo_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
